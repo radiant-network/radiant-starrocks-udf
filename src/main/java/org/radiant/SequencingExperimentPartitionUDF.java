be138d7e6c9c00c43bcd5c44c994d4f0228ee077
Karl Ritchie <ritchie.karl@gmail.com>
2025-05-30 12:00:11 -0400

    :sparkles: feat: SJRA-274 Remove sequencingType to simplify UDF

--- src/main/java/org/radiant/SequencingExperimentPartitionUDF.java
+++ src/main/java/org/radiant/SequencingExperimentPartitionUDF.java
@@ -5,26 +5,20 @@ public class SequencingExperimentPartitionUDF {
     private static final short WGS_TYPE_ID = 1;
     private static final short WXS_TYPE_ID = 2;
 
-    private static final java.util.Map<String, Short> SEQUENCING_TYPE_ALIAS_MAP = java.util.Map.of(
-            "wgs", WGS_TYPE_ID,
-            "wxs", WXS_TYPE_ID
-    );
-
     private static final java.util.Map<Short, Integer> SEQUENCING_TYPE_LIMIT_MAP = java.util.Map.of(
             WGS_TYPE_ID, 100,
             WXS_TYPE_ID, 1000
     );
 
-    public Integer evaluate(String sequencingType, Integer currentPartitionId, Integer currentPartitionCount) {
+    public Integer evaluate(Integer currentPartitionId, Integer currentPartitionCount) {
         // Validate inputs
-        if (sequencingType == null || currentPartitionId == null || currentPartitionCount == null) { return null; }
+        if (currentPartitionId == null || currentPartitionCount == null) { return null; }
 
         // Validate sequencing type
-        Short sequencingTypeId = SEQUENCING_TYPE_ALIAS_MAP.get(sequencingType.toLowerCase());
-        if (sequencingTypeId == null) { return null; }
-        if ((currentPartitionId >> 16) != sequencingTypeId) { return null; }
+        Integer sequencingTypeLimit = SEQUENCING_TYPE_LIMIT_MAP.get((short) (currentPartitionId >> 16));
+        if (sequencingTypeLimit == null) { return null; }
 
-        if (currentPartitionCount >= SEQUENCING_TYPE_LIMIT_MAP.get(sequencingTypeId)) {
+        if (currentPartitionCount >= sequencingTypeLimit) {
             short body = (short) ((currentPartitionId & 0xFFFF) + 1);
             return (currentPartitionId & 0xFFFF0000) | body;
         }
--- src/test/java/org/radiant/SequencingExperimentPartitionUDFTest.java
+++ src/test/java/org/radiant/SequencingExperimentPartitionUDFTest.java
@@ -8,65 +8,51 @@ class SequencingExperimentPartitionUDFTest {
     private SequencingExperimentPartitionUDF udf;
 
     @Test
-    void test_UnsupportedSequencingType() {
+    void testNullValues() {
         udf = new SequencingExperimentPartitionUDF();
-        Integer result = udf.evaluate("unsupported", 0x00020001, 50);
-        Assertions.assertNull(result);
-    }
-
-    @Test
-    void test_wgs_PartitionIdDoesNotMatchSequencingTypeId() {
-        udf = new SequencingExperimentPartitionUDF();
-        Integer result = udf.evaluate("wgs", 0x00020001, 50);
+        Integer result = udf.evaluate(null, 500);
         Assertions.assertNull(result);
     }
 
     @Test
     void test_wgs_ShouldIncrement() {
         udf = new SequencingExperimentPartitionUDF();
-        int result = udf.evaluate("wgs", 0x00010001, 100);
+        int result = udf.evaluate(0x00010001, 100);
         Assertions.assertEquals(0x00010002, result);
     }
 
     @Test
     void test_wgs_ShouldNotIncrement() {
         udf = new SequencingExperimentPartitionUDF();
-        int result = udf.evaluate("wgs", 0x00010001, 99);
+        int result = udf.evaluate(0x00010001, 99);
         Assertions.assertEquals(0x00010001, result);
     }
 
     @Test
     void test_wgs_UpperCase() {
-        SequencingExperimentPartitionUDF udf = new SequencingExperimentPartitionUDF();
-        int result = udf.evaluate("WGS", 0x0001000F, 1500);
-        Assertions.assertEquals(0x00010010, result);
-    }
-
-    @Test
-    void test_wxs_PartitionIdDoesNotMatchSequencingTypeId() {
         udf = new SequencingExperimentPartitionUDF();
-        Integer result = udf.evaluate("wxs", 0x00010001, 500);
-        Assertions.assertNull(result);
+        int result = udf.evaluate(0x0001000F, 1500);
+        Assertions.assertEquals(0x00010010, result);
     }
 
     @Test
     void test_wxs_ShouldIncrement() {
         udf = new SequencingExperimentPartitionUDF();
-        int result = udf.evaluate("wxs", 0x00020001, 1000);
+        int result = udf.evaluate(0x00020001, 1000);
         Assertions.assertEquals(0x00020002, result);
     }
 
     @Test
     void test_wxs_ShouldNotIncrement() {
         udf = new SequencingExperimentPartitionUDF();
-        int result = udf.evaluate("wxs", 0x00020001, 999);
+        int result = udf.evaluate(0x00020001, 999);
         Assertions.assertEquals(0x00020001, result);
     }
 
     @Test
     void test_wxs_UpperCase() {
         SequencingExperimentPartitionUDF udf = new SequencingExperimentPartitionUDF();
-        int result = udf.evaluate("WXS", 0x0002000F, 1500);
+        int result = udf.evaluate(0x0002000F, 1500);
         Assertions.assertEquals(0x00020010, result);
     }
 
